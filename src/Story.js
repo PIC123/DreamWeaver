293afdc381d6c8f4d9e2ec8fb6a2294613c3d6f3
Phil Cherner <pcherner@mit.edu>
2023-10-03 03:46:16 -0400

    remove api keys

--- src/Story.js
+++ src/Story.js
@@ -21,38 +21,17 @@ export default function Story() {
   const oai = new OAI({
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
-    // apiKey: "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", dangerouslyAllowBrowser: true,
   });
 
   const model = new ChatOpenAI({
-    // openAIApiKey: "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",
     openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
     modelName: "gpt-4",
-    // openAIApiKey: process.env.OPENAI_API_KEY,
   });
 
   const messagesEndRef = useRef(null); // Create a ref
   const scrollToBottom = () => {
     messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
   };
-
-//   const messageHistory = [["system", systemTemplate]];
-
-// const model = new OpenAI({openAIApiKey: "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",});
-
-//   const pastMessages = [
-//     new SystemMessage(`Act as a terminal for a zork clone text based dungeon adventure game based in ${setting}. Respond only with descriptions of the environment and react to basic commands like moving in a direction or picking up items. Begin the story in a randomly generated space and describe what the player sees. Only return json objects as responses. The objects should have the parameters \"story-text\", which is just a string of the generated description, \"possible-actions\", which is a list of possible actions that the user can take, \"location\" which is an x,y pair that denotes the Euclidian distance from the starting location in steps with north and east being the positive directions. Also include the parameter \"dall-e-prompt\" which contains a generated prompt for the generative art ai dall-e to produce a digital painting of the current description with lots of details in a hand-drawn style. Keep track of the user's actions in a parameter called \"action-history\" that is a list of the actions that the user has take so far, with the corresponding location that the action happened.`),
-//   ];
-
-//   const memory = new BufferMemory({
-//     chatHistory: new ChatMessageHistory(pastMessages),
-//   });
-
-//   const memory = new BufferMemory({
-//     chatHistory: new ChatMessageHistory(),
-//   });
-
-//   const chain = new ConversationChain({ llm: model, memory: memory,})
 
   const chatPrompt = ChatPromptTemplate.fromMessages(messageHistory);
 
@@ -72,19 +51,11 @@ export default function Story() {
         const parsed = JSON.parse(result.content)
         await setMessages([{ text: parsed["story-text"], sender: 'system' }]);
         await setMessageHistory([...messageHistory, ["ai",parsed["story-text"]]]);
-        // messageHistory.push(["ai",result.content])
         await getImage(parsed["dall-e-prompt"]);
         await setPossibleActions(parsed["possible-actions"]);
         scrollToBottom(); // Scroll to bottom when component mounts
-        // setImgSrc(image_url);
     }
     fetchData();
-    // const result = await chain.invoke({input: input});
-    
-    // const result = await model.predictMessages(starterChatPrompt);
-    // console.log("result", result);
-    // const parsed = JSON.parse(result.content)
-    // setMessages([...messages, { text: parsed["story-text"], sender: 'system' }]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -94,33 +65,20 @@ export default function Story() {
   
   const humanTemplate = "{action}";
 
-
-//   const chatPrompt = ChatPromptTemplate.fromMessages([
-//     ["human", humanTemplate],
-//   ]);
-
-//   const chain = chatPrompt.pipe(model);
-
   // Handler for possible action buttons
   const handleActionClick = async (action) => {
     const chatPrompt = ChatPromptTemplate.fromMessages([...messageHistory,
             ["human", humanTemplate],
           ]);
-    // const result = await chain.invoke({
-    //     setting: setting,
-    //     action: input,
-    //   });
     const formattedChatPrompt = await chatPrompt.formatMessages({
         setting: setting,
         action: action + "and respond ONLY with the JSON defined above",
         });
     await setMessages([...messages, { text: action, sender: 'human' }, { text: "Loading...", sender: 'system' }]);
     const result = await model.predictMessages(formattedChatPrompt);
-    // const result = await chain.invoke({input: input});
     const parsed = JSON.parse(result.content)
     setInput('');
     await setMessages([...messages, { text: action, sender: 'human' }, { text: parsed["story-text"], sender: 'system' }]);
-    // await setMessageHistory([...messageHistory, ["human", input], ["ai",parsed["story-text"]]]);
     messageHistory.push(["human", action]);
     messageHistory.push(["ai",parsed["story-text"]])
     await getImage(parsed["dall-e-prompt"]);
@@ -133,31 +91,22 @@ export default function Story() {
   };
 
   const handleSubmit = async () => {
-    // setMessages([...messages, { text: input, sender: 'user' }]);
-    // const chatModelResult = await chatModel.predict(text);
     const chatPrompt = ChatPromptTemplate.fromMessages([...messageHistory,
             ["human", humanTemplate],
           ]);
-    // const result = await chain.invoke({
-    //     setting: setting,
-    //     action: input,
-    //   });
     const formattedChatPrompt = await chatPrompt.formatMessages({
         setting: setting,
         action: input + "and respond ONLY with the JSON defined above",
         });
     await setMessages([...messages, { text: input, sender: 'human' }, { text: "Loading...", sender: 'system' }]);
     const result = await model.predictMessages(formattedChatPrompt);
-    // const result = await chain.invoke({input: input});
     const parsed = JSON.parse(result.content)
     setInput('');
     await setMessages([...messages, { text: input, sender: 'human' }, { text: parsed["story-text"], sender: 'system' }]);
-    // await setMessageHistory([...messageHistory, ["human", input], ["ai",parsed["story-text"]]]);
     messageHistory.push(["human", input]);
     messageHistory.push(["ai",parsed["story-text"]])
     await getImage(parsed["dall-e-prompt"]);
     await setPossibleActions(parsed["possible-actions"]);
-    // setImgSrc(image_url);
     // Here, you may want to implement a logic to add a system response
   };
 
